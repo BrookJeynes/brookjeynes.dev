e6efc2137fe5c443a2cd362e6732750a2d6d8c47
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2022-09-04 23:33:06 +1000

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
-      <Route path="*" element={<></>} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
